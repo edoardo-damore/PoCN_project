706acd0d6599a54b6681e84fcb0b381928c0ae41
Edoardo D'Amore <edoardo.damore@studenti.unipd.it>
2025-11-19 21:19:20 +0100

    cleaning is now more efficient

--- code/task_42/cleaning.py
+++ code/task_42/cleaning.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.16.5"
+__generated_with = "0.17.8"
 app = marimo.App(
     width="full",
     css_file="/home/edoardo-damore/.local/share/motheme/themes/coldme.css",
@@ -14,260 +14,161 @@ def _():
     import matplotlib.pyplot as plt
     import networkx as nx
     import numpy as np
-    import os
     import polars as pl
-    return mo, os, pl, plt
+
+    from pathlib import Path
+    from pydantic import validate_call
+    return Path, mo, pl, plt
 
 
 @app.cell
-def _(os):
-    # this notebook has to be run from the project directory, not inside the `code` directory
-    rawData_path = os.path.abspath("raw/task_42")
-    data_path = os.path.abspath("data/task_42")
-    if not os.path.isdir(data_path):
-        os.makedirs(data_path)
-    return data_path, rawData_path
-
-
-@app.cell
-def _(mo):
-    mo.md(
-        r"""
-    # Retrieval and cleaning
-    The first step is to retrieve the data and perform an immediate sanitization
-    """
-    )
-    return
-
-
-@app.cell
-def _(pl):
-    def sanitizeColumns(
-        df: pl.DataFrame, sanitizeOptions: dict[str, list[str]] = None
-    ) -> pl.DataFrame:
-        """
-        Sanitization pipeline:
-        `remove_spaces` -> `to_int` -> `to_float`
-
-        Every step is optional.
-        Error handling is not yet supported, so be careful
-
-        Args:
-            df (pl.DataFrame): the DataFrame you would like to sanitize
-            sanitizeOptions (dict[str, list[str]]): a dictionary where each key is a column of `df`, and the corresponding value is a list of sanitization steps (possible: `remove_spaces`, `to_int`, `to_float`)
-
-        Returns:
-            pl.DataFrame: the sanitized `df`
-        """
-        if sanitizeOptions is None:
-            return df
-        for col in sanitizeOptions.keys():
-            if "remove_spaces" in sanitizeOptions[col]:
-                df = df.with_columns(df.get_column(col).str.replace_all(" ", ""))
-            if "to_int" in sanitizeOptions[col]:
-                df = df.with_columns(df.get_column(col).cast(pl.Int32))
-            if "to_float" in sanitizeOptions[col]:
-                df = df.with_columns(df.get_column(col).cast(pl.Float64))
-        return df
-    return (sanitizeColumns,)
-
-
-@app.cell
-def _(pl, sanitizeColumns):
-    def get_rawData(
-        path: str, sanitizeOptions: dict[str, list[str]] = None, **kwargs
-    ) -> pl.DataFrame:
-        rawData = pl.read_csv(path, **kwargs).pipe(
-            sanitizeColumns, sanitizeOptions
-        )
-        return rawData
-    return (get_rawData,)
-
-
-@app.cell
-def _(get_rawData, rawData_path):
-    rawData_nodes_sanitizeOptions = {
-        "node": ["remove_spaces", "to_int"],
-        "layer": ["to_int"],
-        "lat": [],
-        "lon": ["remove_spaces", "to_float"],
-        "zone": ["remove_spaces"],
-        "atcocode": ["remove_spaces"],
-    }
-    rawData_nodes = get_rawData(
-        f"{rawData_path}/nodes.csv", rawData_nodes_sanitizeOptions
-    )
-    rawData_nodes
-    return (rawData_nodes,)
-
-
-@app.cell
-def _(get_rawData, rawData_path):
-    rawData_stops_sanitizeOptions = None
-    rawData_stops = get_rawData(
-        f"{rawData_path}/NaPTAN_NPTG/Stops.csv",
-        rawData_stops_sanitizeOptions,
-        encoding="utf8-lossy",
-        schema_overrides={"ATCOCode": str},
-    )
-    rawData_stops
-    return (rawData_stops,)
-
-
-@app.cell
-def _(get_rawData, rawData_path):
-    rawData_cities_sanitizeOptions = None
-    rawData_cities = get_rawData(
-        f"{rawData_path}/united-kingdom-cities-by-population-2025.csv",
-        rawData_cities_sanitizeOptions,
-    )
-    rawData_cities
-    return (rawData_cities,)
-
-
-@app.cell
-def _(get_rawData, rawData_path):
-    rawData_groups_sanitizeOptions = None
-    rawData_groups = get_rawData(
-        f"{rawData_path}/NaPTAN_NPTG/Groups.csv",
-        rawData_groups_sanitizeOptions,
-        encoding="utf8-lossy",
-    )
-    rawData_groups
-    return (rawData_groups,)
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""# Creating the nodes""")
-    return
-
-
-@app.cell
-def _(pl):
-    def get_nodes(rawData: dict[str, pl.DataFrame]):
-        stops = (
-            rawData["nodes"]
-            .join(rawData["stops"], left_on="atcocode", right_on="ATCOCode")
-            .join(rawData["cities"], left_on="ParentLocality", right_on="city")
-            .filter(pl.col("pop2025") > 50000)
-            .select(["node", "layer", "lat", "lon", "atcocode", "ParentLocality"])
-        )
-        return stops
-    return (get_nodes,)
+def _(Path):
+    BASE_DIR = Path(__file__).parent.parent.parent
+    RAW_DIR = BASE_DIR / "raw" / "task_42"
+    DATA_DIR = BASE_DIR / "data" / "task_42"
+    if not DATA_DIR.exists() or not DATA_DIR.is_dir():
+        DATA_DIR.mkdir()
+    return (RAW_DIR,)
 
 
 @app.cell(hide_code=True)
-def _():
-    """
-    def get_nodes(rawData: dict[str, pl.DataFrame]):
-        # let's extract first the cities with more than 50k inhabitants
-        # and let's create the corresponding regex pattern
-        # AIDED WITH CLAUDE SONNET 4.5
-        cities_list = (
-            rawData["cities"]
-            .filter(pl.col("pop2025") > 50000)
-            .get_column("city")
-            .to_list()
-        )
-        cities_pattern = "|".join(cities_list)
-
-        # let's extract the stops present in the corresponding cities
-        stops = (
-            rawData["nodes"]
-            .join(
-                rawData["stops"].with_columns(
-                    pl.concat_str(
-                        [
-                            # pl.col("CommonName"),
-                            # pl.col("Identifier"),
-                            pl.col("NatGazLocality"),
-                            pl.col("ParentLocality"),
-                            pl.col("GrandParentLocality"),
-                        ],
-                        separator=" - ",
-                    ).alias("Locality")
-                ),
-                left_on="atcocode",
-                right_on="ATCOCode",
-            )
-            .filter(pl.col("Locality").str.contains(cities_pattern))
-            .select(["node", "layer", "lat", "lon", "atcocode", "Locality"])
-        )
-
-        # and now the groups
-        groups = (
-            rawData["nodes"]
-            .join(rawData["groups"], left_on="atcocode", right_on="GroupID")
-            .filter(pl.col("GroupName").str.contains(cities_pattern))
-            .with_columns(pl.col("GroupName").alias("Locality"))
-            .select(["node", "layer", "lat", "lon", "atcocode", "Locality"])
-        )
-        return pl.concat([stops, groups])
-    """
+def _(mo):
+    mo.md(r"""
+    # Retrieval and cleaning
+    The first step is to retrieve the data and perform an immediate sanitization
+    """)
     return
 
 
 @app.cell
-def _(get_nodes, rawData_cities, rawData_groups, rawData_nodes, rawData_stops):
-    nodes = get_nodes(
-        {
-            "nodes": rawData_nodes,
-            "stops": rawData_stops,
-            "groups": rawData_groups,
-            "cities": rawData_cities,
-        }
-    )
-    return (nodes,)
+def _(Path, pl):
+    class RawDataStorer:
+        """
+        Simple class to store and preprocess all the original data tables.
+
+        # Parameters
+        :param paths: the paths where the tables are stored. A LazyFrame is created for each table.
+        :type paths: dict[str, Path]
+        :param configs: lists of Polars' expressions that are applyed to the corresponding LazyFrames.
+        :type configs: dict[str, list[pl.Expr]]
+        """
+
+        def __init__(
+            self,
+            paths: dict[str, Path],
+            configs: dict[str, list[pl.Expr]],
+        ):
+            self.nodes = self._load(paths["nodes"], configs["nodes"])
+            self.stops = self._load(paths["stops"], configs["stops"])
+            self.cities = self._load(paths["cities"], configs["cities"])
+            self.groups = self._load(paths["groups"], configs["groups"])
+            self.edges = self._load(paths["edges"], configs["edges"])
+            return
+
+        def _load(self, path: Path, config: list[pl.Expr] | None) -> pl.LazyFrame:
+            lazy_df = pl.scan_csv(
+                path, encoding="utf8-lossy", infer_schema_length=10000
+            )
+            return self._sanitize(lazy_df, config)
+
+        def _sanitize(
+            self, lazy_df: pl.LazyFrame, config: list[pl.Expr] | None
+        ) -> pl.LazyFrame:
+            """ """
+            lazy_df = lazy_df.with_columns(config)
+            return lazy_df
+    return (RawDataStorer,)
 
 
 @app.cell
-def _(nodes, pl, plt, rawData_cities):
-    cities = rawData_cities.filter(pl.col("pop2025") > 50000)
+def _(RAW_DIR, RawDataStorer, pl):
+    raw_data_paths = {
+        "nodes": RAW_DIR / "nodes.csv",
+        "stops": RAW_DIR / "NaPTAN_NPTG" / "Stops.csv",
+        "cities": RAW_DIR / "united-kingdom-cities-by-population-2025.csv",
+        "groups": RAW_DIR / "NaPTAN_NPTG" / "Groups.csv",
+        "edges": RAW_DIR / "edges.csv",
+    }
+    raw_data_configs = {
+        "nodes": [
+            pl.col("node").str.strip_chars().cast(pl.Int32),
+            pl.col("layer").cast(pl.Int32),
+            pl.col("lon").str.strip_chars().cast(pl.Float64),
+            pl.col("zone").str.strip_chars(),
+            pl.col("atcocode").str.strip_chars(),
+        ],
+        "stops": [pl.col("ATCOCode").cast(pl.String)],
+        "cities": None,
+        "groups": None,
+        "edges": [
+            pl.col("ori_node").str.strip_chars().cast(pl.Int32),
+            pl.col("des_node").str.strip_chars().cast(pl.Int32),
+            pl.col("minutes").str.strip_chars().cast(pl.Int32),
+            pl.col("km").str.strip_chars().cast(pl.Float64),
+        ],
+    }
+    raw_data = RawDataStorer(raw_data_paths, raw_data_configs)
+    raw_data.edges.collect().head(10)
+    return (raw_data,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Creating the nodes
+    """)
+    return
+
+
+@app.cell
+def _(pl, raw_data):
+    big_cities = raw_data.cities.filter(pl.col("pop2025") > 50000)
+    all_nodes = raw_data.nodes.join(
+        raw_data.stops, left_on="atcocode", right_on="ATCOCode"
+    )
+    nodes = pl.concat(
+        [
+            raw_data.nodes.join(
+                raw_data.stops, left_on="atcocode", right_on="ATCOCode"
+            )
+            .join(big_cities, left_on=f"{locality_type}Locality", right_on="city")
+            .rename({f"{locality_type}Locality": "city"})
+            .select(["node", "layer", "lat", "lon", "atcocode", "city"])
+            for locality_type in ["GrandParent", "Parent", "NatGaz"]
+        ]
+    )
+    nodes.collect()
+    return big_cities, nodes
+
+
+@app.cell
+def _(big_cities, nodes, plt):
+    cities = big_cities.collect()
     fig, ax = plt.subplots(figsize=(6, 6))
-    ax.scatter(nodes["lon"], nodes["lat"], marker=".", s=1)
+    ax.scatter(nodes["lon"], nodes["lat"], marker=".", color="c", s=1)
     ax.scatter(cities["longitude"], cities["latitude"], marker=".", color="r", s=5)
 
     plt.show()
     return
 
 
-@app.cell
-def _(nodes):
-    nodes
-    return
-
-
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""# Selecting the edges""")
+    mo.md(r"""
+    # Selecting the edges
+    """)
     return
 
 
 @app.cell
-def _(get_rawData, rawData_path):
-    rawData_edges_sanitizeOptions = {
-        "ori_node": ["remove_spaces", "to_int"],
-        "des_node": ["remove_spaces", "to_int"],
-        "minutes": ["remove_spaces", "to_int"],
-        "km": ["remove_spaces", "to_float"],
-    }
-    rawData_edges = get_rawData(
-        f"{rawData_path}/edges.csv", rawData_edges_sanitizeOptions
-    )
-    rawData_edges
-    return (rawData_edges,)
-
-
-@app.cell
-def _(nodes, pl, rawData_edges):
+def _(nodes, raw_data):
     edges = (
-        rawData_edges.join(
+        raw_data.edges.join(
             nodes, left_on=["ori_node", "ori_layer"], right_on=["node", "layer"]
         )
-        .with_columns(pl.col("ParentLocality").alias("ori_city"))
+        .rename({"city": "ori_city"})
         .join(nodes, left_on=["des_node", "des_layer"], right_on=["node", "layer"])
-        .with_columns(pl.col("ParentLocality").alias("des_city"))
+        .rename({"city": "des_city"})
         .select(
             [
                 "ori_node",
@@ -281,13 +182,15 @@ def _(nodes, pl, rawData_edges):
             ]
         )
     )
-    edges
+    edges.collect()
     return (edges,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""# Divide in cities""")
+    mo.md(r"""
+    # Divide in cities
+    """)
     return
 
 
@@ -317,11 +220,7 @@ def _(pl):
                 [key_values, pl.arange(1, key_values.shape[0] + 1, eager=True)],
                 schema=[col, "newKey"],
             )
-        mapped_data = (
-            data.join(mapping, on=col)
-            .drop(col)
-            .rename({"newKey": col})
-        )
+        mapped_data = data.join(mapping, on=col).drop(col).rename({"newKey": col})
         if return_mapping:
             return mapped_data, mapping
         return mapped_data
@@ -371,14 +270,8 @@ def _(data_path, os, pl, remap_key):
 
 
 @app.cell
-def _(divide_by_city, edges, nodes, rawData_cities):
-    divide_by_city({"cities": rawData_cities, "nodes": nodes, "edges": edges})
-    return
-
-
-@app.cell
-def _():
-    print("a")
+def _(divide_by_city, edges, nodes, raw_data_cities):
+    divide_by_city({"cities": raw_data_cities, "nodes": nodes, "edges": edges})
     return
 
 
